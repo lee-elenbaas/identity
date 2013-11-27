4325cf4b5d4ddd61291eb379c54aa12bdb6ecf1d
Lee Elenbaas <lee@elenbaas.org.il>
2013-11-27 22:32:05 +0200

    start of reordering

---
+++ sample/identity-usage.js
@@ -0,0 +1,18 @@
+"use strict";
+
+requirejs.config({
+	paths: {
+		jquery: [
+			"http://code.jquery.com/jquery-2.0.3.min",
+			"../lib/jquery-2.0.3.min.js"
+		],
+		knockout: [
+			"http://knockoutjs.com/downloads/knockout-3.0.0",
+			"../lib/knockout-3.0.0.js"
+		],
+		"jquery-knockout-plugin": "../../src/jquery-knockout-plugin",
+		"identity": "../src/js"
+	}
+});
+
+requirejs(["identity/host"]);
